76216435e707ab5aa61914b7d6bf73fad7519117
rainbow31499 <85587259+rainbow31499@users.noreply.github.com>
2022-07-12 08:19:44 -0700

    Add files via upload

--- week-6/data_creator.py
+++ week-6/data_creator.py
@@ -2,14 +2,14 @@ import random
 from datetime import datetime
 import pandas as pd
 
-# A sample data of 30000000 observations of soil temperatures taken at 538 locations around the world
+# A sample data of 40000000 observations of soil temperatures taken at 538 locations around the world
 
 random.seed(1030)
 
-indices = list(range(30000000))
+indices = list(range(40000000))
 
 location = [random.randrange(1,539) for i in indices]
-location_temperature = {i:random.uniform(-10,30) for i in range(1,5393)}
+location_temperature = {i:random.uniform(-10,30) for i in range(1,539)}
 temperature = [random.normalvariate(location_temperature[location[i]], 4) for i in indices]
 depth = [random.uniform(15,35) for i in indices]
 time_taken = [datetime.fromtimestamp(random.uniform(datetime(2022,6,1,0,0,0).timestamp(), \
@@ -18,7 +18,6 @@ time_taken = [datetime.fromtimestamp(random.uniform(datetime(2022,6,1,0,0,0).tim
 data = pd.DataFrame({"location": location, \
                      "temperature": temperature, \
                      "depth": depth, \
-                     "time_taken": time_taken}, \
-                     index = indices)
+                     "time_taken": time_taken})
 
-data.to_csv("sample_data.csv")
+data.to_csv("sample_data.csv", index=False)
